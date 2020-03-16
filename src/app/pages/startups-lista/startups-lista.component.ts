525a515a1cc58724089472a22174b35a857cc69a
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-03-15 22:13:47 -0300

    adicionar lista startups

---
+++ src/app/pages/startups-lista/startups-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startups-lista',
+  templateUrl: './startups-lista.component.html',
+  styleUrls: ['./startups-lista.component.css']
+})
+export class StartupsListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
