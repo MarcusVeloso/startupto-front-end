736fdfe47f064c91de98beba98de8a369dd64049
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-03-15 22:14:35 -0300

    adicionar lista noticias

---
+++ src/app/pages/noticias-lista/noticias-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias-lista',
+  templateUrl: './noticias-lista.component.html',
+  styleUrls: ['./noticias-lista.component.css']
+})
+export class NoticiasListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
